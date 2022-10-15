27a56ba7126f3d662450b4fc2ba5f0288f653074
daniel <duqingqing@dayuwuxian.com>
2022-10-15 20:01:53 +0800

    feat:support thread

--- model/frame_config.py
+++ model/frame_config.py
@@ -3,7 +3,7 @@ class FrameConfig(object):
         相框配置信息
     """
     # 相框像素大小
-    padding_width = 150
+    padding_width = 100
     # 相框底色
     padding_color = "white"
     # 相框底部批注颜色
--- process/process_frame.py
+++ process/process_frame.py
@@ -3,9 +3,11 @@ from loguru import logger
 from action.gen_frame import GenFrameAction
 from model.context import Context
 from util.file_util import *
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def process_frame(input_dir, out_dir):
+    context_list = []
     try:
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
@@ -13,8 +15,23 @@ def process_frame(input_dir, out_dir):
         for image in get_images(input_dir):
             logger.info("process {}", image)
             context = Context(image, out_dir)
-            action = GenFrameAction(context)
-            action.do_action()
-        print("success!\n result file path: " + out_dir)
+            context_list.append(context)
+
+        with ThreadPoolExecutor(max_workers=8) as t:
+            job_list = []
+            for context in context_list:
+                job = t.submit(action, context)
+                job_list.append(job)
+        index = 0
+        for future in as_completed(job_list):
+            future.result()
+            index += 1
+            if (index == len(job_list)):
+                print("success!\n result file path: " + out_dir)
     except Exception as e:
         logger.error("process error,{}", e)
+
+
+def action(context):
+    action = GenFrameAction(context)
+    action.do_action()
